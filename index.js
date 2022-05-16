430f973598ea480e0866aca183fb608983de8097
Erica Guadalupe <guadalupeerica@yahoo.com>
2022-05-16 12:20:41 -0500

    Add: Add for loop to play 5 rounds of game

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Create function for the cpu to make a move
+//Below is the function for the cpu to make its move. 
 
 function cpuPlays() {
     const move = Math.floor(Math.random() * 3) + 1;
@@ -12,17 +12,21 @@ function cpuPlays() {
     }
 }
 
-// Create function for the game with the parameters for player turn and cpu turn
+// Below is the function for the player input, and what the results will be once calculated with the cpu input. 
 
-function gameOne(playerTurn, cpuTurn) {
-    let result = 'You win!!'
+function makeMove(playerTurn, cpuTurn) {
 
     if (playerTurn === 'rock' && cpuTurn === 'paper' || playerTurn === 'paper' && cpuTurn === 'scissor' || playerTurn === 'scissor' && cpuTurn === 'rock') {
         return console.log('You lose!');
     } else {
-        return result;
+        return console.log('You win!');
     }
 }
 
-//Make default variables for rock, paper scissor 
-// Create conditions for each variables that will output a win/lose string message depending on the move made
+// Below is a loop so there can be 5 rounds of the game. 
+
+function game() {
+    for (var i = 0; i < 5; i++) {
+        makeMove(prompt('Make your move! Rock, paper or scissor?'), cpuPlays()); 
+    }
+}
