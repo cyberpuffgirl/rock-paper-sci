d126a0584f5cc7e3fbb163329a4b96ca8c179f09
Erica Guadalupe <guadalupeerica@yahoo.com>
2022-05-16 17:26:04 -0500

    Add: Add variables for storing scores

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function cpuPlays() {
     }
 }
 
-// Below is the function for the player input, and what the results will be once calculated with the cpu input. 
+//Below is the function for the player input, and what the results will be once calculated with the cpu input. 
 
 function makeMove(playerTurn, cpuTurn) {
     let lose = 'You lose!'
-    let win = 'You win!!'
+    let win = 'You win!'
 
     if (playerTurn === 'rock' || playerTurn === 'Rock') {
         if (cpuTurn === 'paper') {
@@ -34,7 +34,7 @@ function makeMove(playerTurn, cpuTurn) {
         if (cpuTurn === 'rock') {
             console.log(lose);
         } else {
-            console.log(win);
+            console.log(win);;
         }
     } else if (playerTurn === '' || playerTurn === null) { // Will need to edit this later to have the loop break if one of these answers is provided. 
         console.log('Cancelled');
@@ -43,10 +43,17 @@ function makeMove(playerTurn, cpuTurn) {
     }
 }
 
-// Below is a loop so there can be 5 rounds of the game. 
+//Below are the variables I will need to store and report scores
+
+let playerScore = 0;
+let cpuScore = 0;
+let totalScore = playerScore + cpuScore;
+
+//Below is a loop so there can be 5 rounds of the game
 
 function game() {
     for (var i = 0; i < 5; i++) {
         makeMove(prompt('Make your move! Rock, paper or scissor?'), cpuPlays()); 
+        if 
     }
 }
