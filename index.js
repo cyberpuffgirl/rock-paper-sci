97eee7fb22f895281be0e4c8023c5d93aa91035c
Erica Guadalupe <guadalupeerica@yahoo.com>
2022-05-16 18:03:48 -0500

    Edit: edit makeMove function to add return value for cpu and player score

--- index.js
+++ index.js
@@ -12,48 +12,55 @@ function cpuPlays() {
     }
 }
 
+//Below are the variables I will need to store and report scores.
+
+let playerScore = 1;
+let cpuScore = 1;
+let totalScore;
+
 //Below is the function for the player input, and what the results will be once calculated with the cpu input. 
 
 function makeMove(playerTurn, cpuTurn) {
-    let lose = 'You lose!'
-    let win = 'You win!'
+    let lose = 'Round lost!'
+    let win = 'Round won!'
 
     if (playerTurn === 'rock' || playerTurn === 'Rock') {
         if (cpuTurn === 'paper') {
             console.log(lose);
+            return console.log('CPU points:' + ' ' + cpuScore++);
         } else {
             console.log(win);
+            return console.log('Human points:' + ' ' + playerScore++);
         }
     } else if (playerTurn === 'paper' || playerTurn === 'Paper') {
         if (cpuTurn === 'scissor') {
             console.log(lose);
+            return console.log('CPU points:' + ' ' + cpuScore++);
         } else {
             console.log(win);
+            return console.log('Human points:' + ' ' + playerScore++);
         }
     } else if (playerTurn === 'scissor' || playerTurn === 'Scissor') {
         if (cpuTurn === 'rock') {
             console.log(lose);
+            return console.log('CPU points:' + ' ' + cpuScore++);
         } else {
-            console.log(win);;
+            console.log(win);
+            return console.log('Human points:' + ' ' + playerScore++);
         }
     } else if (playerTurn === '' || playerTurn === null) { // Will need to edit this later to have the loop break if one of these answers is provided. 
         console.log('Cancelled');
     } else {
-        console.log('That is not a valid option.', lose);
-    }
+        console.log('That is not a valid answer, but it still counts as a move. Round lost. )-:')
+        return console.log('CPU points:' + ' ' + cpuScore++);
+    };
+    return totalScore;
 }
-
-//Below are the variables I will need to store and report scores
-
-let playerScore = 0;
-let cpuScore = 0;
-let totalScore = playerScore + cpuScore;
 
 //Below is a loop so there can be 5 rounds of the game
 
 function game() {
     for (var i = 0; i < 5; i++) {
         makeMove(prompt('Make your move! Rock, paper or scissor?'), cpuPlays()); 
-        if 
-    }
+    };  
 }
