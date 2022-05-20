7b6d4c69a890861a258c388bc9cf490237cdba96
Erica Guadalupe <guadalupeerica@yahoo.com>
2022-05-20 15:35:04 -0500

    Edit: edit game and makeMove functions. This will allow us to store the results from makeMove into a variable within the game function.

--- index.js
+++ index.js
@@ -12,55 +12,50 @@ function cpuPlays() {
     }
 }
 
-//Below are the variables I will need to store and report scores.
-
-let playerScore = 1;
-let cpuScore = 1;
-let totalScore;
-
-//Below is the function for the player input, and what the results will be once calculated with the cpu input. 
+//Below is the function for the player input and cpu input. The result of the round will either return humanRound for a human win or cpuRound for a cpu win. 
 
 function makeMove(playerTurn, cpuTurn) {
-    let lose = 'Round lost!'
-    let win = 'Round won!'
+    let humanRound = true; 
 
     if (playerTurn === 'rock' || playerTurn === 'Rock') {
         if (cpuTurn === 'paper') {
-            console.log(lose);
-            return console.log('CPU points:' + ' ' + cpuScore++);
+            return humanRound = false; 
         } else {
-            console.log(win);
-            return console.log('Human points:' + ' ' + playerScore++);
+            return humanRound;
         }
     } else if (playerTurn === 'paper' || playerTurn === 'Paper') {
         if (cpuTurn === 'scissor') {
-            console.log(lose);
-            return console.log('CPU points:' + ' ' + cpuScore++);
+            return humanRound = false;
         } else {
-            console.log(win);
-            return console.log('Human points:' + ' ' + playerScore++);
+            return humanRound;
         }
     } else if (playerTurn === 'scissor' || playerTurn === 'Scissor') {
         if (cpuTurn === 'rock') {
-            console.log(lose);
-            return console.log('CPU points:' + ' ' + cpuScore++);
+            return humanRound = false;
         } else {
-            console.log(win);
-            return console.log('Human points:' + ' ' + playerScore++);
+            return humanRound;
         }
     } else if (playerTurn === '' || playerTurn === null) { // Will need to edit this later to have the loop break if one of these answers is provided. 
         console.log('Cancelled');
     } else {
-        console.log('That is not a valid answer, but it still counts as a move. Round lost. )-:')
-        return console.log('CPU points:' + ' ' + cpuScore++);
+        return humanRound = false;
     };
-    return totalScore;
 }
+
 
 //Below is a loop so there can be 5 rounds of the game
 
 function game() {
-    for (var i = 0; i < 5; i++) {
-        makeMove(prompt('Make your move! Rock, paper or scissor?'), cpuPlays()); 
+    let humanScore = 0;
+
+   for (var i = 0; i < 5; i++) {
+    let round = makeMove(prompt('Make your move! Rock, paper or scissor?'), cpuPlays());
+
+    if (round === true) {
+        console.log('Human wins!' + ' ' + 'Your score:' + ' ' + ++humanScore);
+    } else {
+        console.log('CPU wins!');
+    }
     };  
 }
+
